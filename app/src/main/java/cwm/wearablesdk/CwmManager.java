df164744fb7962c2803f785dc0be62ee67eeae35
berniechen <bernie.chen@cyweemotion.com>
2017-11-06 10:50:12 +0800

    add tabata resume command

--- app/src/main/java/cwm/wearablesdk/CwmManager.java
+++ app/src/main/java/cwm/wearablesdk/CwmManager.java
@@ -56,7 +56,8 @@ public class CwmManager{
         TABATA_ACTION_ITEM,
         TABATA_ACTION_START,
         TABATA_ACTION_END,
-        TABATA_DONE
+        TABATA_DONE,
+        TABATA_RESUME
     };
     enum FLASH_SYNC_TYPE{
         SYNC_START,
@@ -590,6 +591,15 @@ public class CwmManager{
                byte[] command = new byte[9];
                /********************************************************************************/
                jniMgr.getTabataCommand(ITEMS.TABATA_DONE.ordinal(), 0, 0, 0, command);
+               /********************************************************************************/
+               if((mConnectStatus != false)){
+                   mService.writeRXCharacteristic(command);
+               }
+           }
+           else if(operate == ITEMS.TABATA_RESUME.ordinal()){
+               byte[] command = new byte[9];
+               /********************************************************************************/
+               jniMgr.getTabataCommand(ITEMS.TABATA_RESUME.ordinal(), 0, 0, 0, command);
                /********************************************************************************/
                if((mConnectStatus != false)){
                    mService.writeRXCharacteristic(command);
--- app/src/main/jni/cwmWearable.c
+++ app/src/main/jni/cwmWearable.c
@@ -758,6 +758,25 @@ JNIEXPORT void JNICALL Java_cwm_wearablesdk_JniManager_getTabataCommand
                  (*env)->SetByteArrayRegion(env, output, 0, 9, txData);
                  free(txData);
           }
+          else if(operate == 12){ //tabata resume
+
+                 txData[0] = (jbyte)0xE6;
+                 txData[1] = (jbyte)0x90;
+                 txData[2] = (jbyte)0x09;
+                 txData[3] = (jbyte)0x17;
+                 txData[4] = (jbyte)0xC; // tabata flag
+                 txData[5] = (jbyte)0x00;
+                 txData[6] = (jbyte)0x00;
+                 txData[7] = (jbyte)0x00;
+
+                 jint checksum = txData[0]+txData[1]+txData[2]+txData[3]+txData[4]+txData[5]+
+                                txData[6]+txData[7];
+
+                 txData[8] = (jbyte)checksum;
+
+                 (*env)->SetByteArrayRegion(env, output, 0, 9, txData);
+                 free(txData);
+          }
 }
 JNIEXPORT void JNICALL Java_cwm_wearablesdk_JniManager_getReadFlashCommand
 (JNIEnv *env, jobject jobj, jint type, jbyteArray output)
